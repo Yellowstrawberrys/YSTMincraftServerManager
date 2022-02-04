2b07d7885078a824a9c7b31bbc8efc025665b55c
Yellowstrawberrys <77413533+Yellowstrawberrys@users.noreply.github.com>
2022-02-04 22:45:36 +0100

    Delete src/main directory

--- src/main/java/cf/thdisstudio/minecraftservermanager/CurrentData.java
+++
@@ -1,8 +0,0 @@
-package cf.thdisstudio.minecraftservermanager;
-
-import java.io.File;
-
-public class CurrentData {
-    public static File selectedServer = null;
-    public static Process CurrentServer = null;
-}
--- src/main/java/cf/thdisstudio/minecraftservermanager/Data/ServerType.java
+++
@@ -1,7 +0,0 @@
-package cf.thdisstudio.minecraftservermanager.Data;
-
-public enum ServerType {
-    Vanilla,
-    Bukkit,
-    Spigot
-}
